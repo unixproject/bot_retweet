efccfc8bc8c6893d8cf71aa45ee85e85251c9a4c
XXXXXXXX <31022461+unixproject@users.noreply.github.com>
2017-11-03 21:07:08 -0500

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,6 @@
+# Twitter application consumer keys and access tokens.
+# Remember, keep this information private.
+consumer_key = 'Dxg2waElrMDKGBTlFKMmffHwj'
+consumer_secret = 'Qr6RdLjXMBzg1S66hlHxTHhohvzGqNXDemKesfl2NIAYhxvvZo'
+access_token = '895864411516059649-tjUeEGk1QVCpGRbzJXFLCGFyDHE5cZb'
+access_token_secret = '9Ai01Acf21IVy4gXWjcbbcWUpbt9WAaeH5GndXkRHb7NI'
