28529b603975168e865c71bef3922a8651ae1b51
XXXXXXXX <31022461+unixproject@users.noreply.github.com>
2017-11-13 17:07:09 -0600

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 # Twitter application consumer keys and access tokens.
 # Remember, keep this information private.
-consumer_key = 'Dxg2waElrMDKGBTlFKMmffHwj'
-consumer_secret = 'Qr6RdLjXMBzg1S66hlHxTHhohvzGqNXDemKesfl2NIAYhxvvZo'
-access_token = '895864411516059649-tjUeEGk1QVCpGRbzJXFLCGFyDHE5cZb'
-access_token_secret = '9Ai01Acf21IVy4gXWjcbbcWUpbt9WAaeH5GndXkRHb7NI'
+consumer_key = 'R0h4CkFAiTUAreMPbKfVhsUb8'
+consumer_secret = 'O1yiaMa7UdR8ceG5ZtfNalIS2TJfNz7swrcQeAbpEpTUn13SF8'
+access_token = '900423041313906688-irGFoKSg87DdkQ2mYCrmzxb4NVnhbCr'
+access_token_secret = 'tWb8BZSLlBUfQPPO9oLAySSrnZDrE01KTwumlUsQ2O9zS'
